67eb01aa50e18c7c10efb3dfe54fb40b05c32e5a
OnofreTZK <taraujonofre@gmail.com>
2025-02-06 16:38:37 -0300

    Initial setup for Cmdliner

--- src/chat.ml
+++ src/chat.ml
@@ -1,38 +1,67 @@
 open Server
 open Client
+open Cmdliner
 
-type execution_mode = Server | Client | Help
+let server_cmd =
+  let doc = "Run as a server side." in
+  let workflow = fun () -> () |> start_server |> Lwt_main.run in
+  let term = Term.(const workflow $ const ()) in
+  let info = Cmd.info "server" ~doc in
+  Cmd.v info term
 
-let mode_of_string_opt = function
-  | "server" | "--server" -> Some Server
-  | "client" | "--client" -> Some Client
-  | "help" | "--help" -> Some Help
-  | _ -> None
-
-let print_help () =
-  let help_string =
-    "\n\
-     Usage:\n\
-    \ dune exec chat <EXECUTION_MODE>\n\n\
-     Modes:\n\
-    \ --server => Run as a server\n\
-    \ --client => Run as a client\n\
-    \ --help   => Print this menu"
+let client_cmd =
+  let hostname_conv =
+    let parse s =
+      let is_valid_inet_addr =
+        try
+          Unix.inet_addr_of_string s |> ignore;
+          true
+        with _ -> false
+      in
+      if is_valid_inet_addr then Ok s else Error (`Msg "Invalid hostname.")
+    in
+    Arg.conv (parse, Format.pp_print_string)
   in
-  print_endline help_string
+  let hostname_arg =
+    let doc = "Server hostname required for establish a connection." in
+    Arg.(
+      required
+      & opt (some hostname_conv) None
+      & info [ "h"; "hostname" ] ~docv:"<HOSTNAME>" ~doc)
+  in
+  let port_number_conv =
+    let parse s =
+      let is_valid_port, port =
+        try
+          int_of_string s |> ignore;
+          (true, port)
+        with _ -> (false, 0)
+      in
+      if is_valid_port then Ok port
+      else Error (`Msg "Unable to parse the port number.")
+    in
+    Arg.conv (parse, Format.pp_print_int)
+  in
+  let port_number_arg =
+    let doc = "Port number where the server is listening." in
+    Arg.(
+      required
+      & opt (some port_number_conv) None
+      & info [ "p"; "port" ] ~docv:"<PORT_NUMBER>" ~doc)
+  in
+  let doc = "Run as a client side." in
+  let workflow _hostname _port =
+    start_client "127.0.0.1" 8090 |> Lwt_main.run
+  in
+  let term = Term.(const workflow $ hostname_arg $ port_number_arg) in
+  let info = Cmd.info "client" ~doc ~man:[] in
+  Cmd.v info term
 
-let main argv =
-  match argv |> Array.length with
-  | 2 -> (
-      match argv.(1) |> mode_of_string_opt with
-      | Some Server -> () |> start_server |> Lwt_main.run
-      | Some Client -> start_client "127.0.0.1" 8090 |> Lwt_main.run
-      | Some Help -> print_help ()
-      | None -> print_endline "Invalid execution mode." |> print_help)
-  | inputs when inputs > 2 ->
-      print_endline "Too many inputs, choose only one execution mode."
-      |> print_help
-  | _ -> print_endline "No execution mode selected." |> print_help
-;;
+let main_cmd =
+  let doc = "A simple one on one chat." in
+  let info = Cmd.info "chat" ~version:"0.0.1" ~doc in
+  (*let default = Term.(const print_help $ const ()) in*)
+  let default = Term.(ret (const (`Help (`Pager, None)))) in
+  Cmd.group info ~default [ server_cmd; client_cmd ]
 
-main Sys.argv
+let () = exit @@ Cmd.eval main_cmd
